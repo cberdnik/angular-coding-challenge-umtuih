4031dbad2e48a7fef97c0b45c77d10f5ce080215
cberdnik <52919438+cberdnik@users.noreply.github.com>
2022-12-08 15:30:48 +0100

    add unit testing

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,33 @@
 import './polyfills';
-
-import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+// import { platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
+// import './test/jasmine-setup';
+// import 'jasmine-core/lib/jasmine-core/jasmine-html.js';
+//import 'jasmine-core/lib/jasmine-core/boot.js';
+// import './test.ts';
 
 import { AppModule } from './app/app.module';
 
-platformBrowserDynamic().bootstrapModule(AppModule).then(ref => {
-  // Ensure Angular destroys itself on hot reloads.
-  if (window['ngRef']) {
-    window['ngRef'].destroy();
-  }
-  window['ngRef'] = ref;
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .then((ref) => {
+    // Ensure Angular destroys itself on hot reloads.
+    if (window['ngRef']) {
+      window['ngRef'].destroy();
+    }
+    window['ngRef'] = ref;
 
-  // Otherwise, log the boot error
-}).catch(err => console.error(err));
+    // Otherwise, log the boot error
+  })
+  .catch((err) => console.error(err));
+
+// (function bootstrap() {
+//   if (window['jasmineRef']) {
+//     location.reload();
+
+//     return;
+//   }
+
+//   window.onload(new Event('anything'));
+//   window['jasmineRef'] = jasmine.getEnv();
+// })();
